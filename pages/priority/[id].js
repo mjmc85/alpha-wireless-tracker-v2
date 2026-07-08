f54f74e69a82c46f92f9d72e2781c7150ce745a0
mjmc85 <mjmcassey@hotmail.com>
2026-07-08 14:15:32 +0100

    Update [id].js

--- pages/priority/[id].js
+++ pages/priority/[id].js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import Layout from "../../components/Layout"
 import { supabase } from "../../lib/supabase"
-
+ 
 export default function PriorityDetail() {
   const router = useRouter()
   const { id } = router.query
